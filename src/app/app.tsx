5b71e17f43336b762c5d2ea52a9ec5c451c2f82c
duyutao <duyutao@pwrd.com>
2021-02-25 15:36:17 +0800

    1. 调整日历列表的宽度 2. 隐藏查看者日程删除的选择项 3. 修复月视图日程列表的 focus  态 4. 调整日程详情参与者的名字显示逻辑

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,138 +1,137 @@
-import "./app.less"
+import "./app.less";
 
 import { Component, Vue } from "vue-property-decorator";
-import { Editor, EditorContent} from 'tiptap';
-import Bold from '@/extensions/Bold';
-import Placeholder from '@/extensions/Placeholder';
-import History from '@/extensions/History';
-import Blockquote from '@/extensions/Blockquote';
-import BulletList from '@/extensions/BulletList';
-import HardBreak from '@/extensions/HardBreak';
-import Heading from '@/extensions/Heading';
-import Image from '@/extensions/Image';
-import ListItem from '@/extensions/ListItem';
-import OrderedList from '@/extensions/OrderedList';
-import HorizontalRule from '@/extensions/HorizontalRule';
-import LineHeight from '@/extensions/LineHeight';
-import { Table, TableHeader, TableRow, TableCell } from '@/extensions/Table';
-import Link from '@/extensions/Link';
-import FontSize from '@/extensions/FontSize';
-import FontFamily from '@/extensions/FontFamily';
-import FontColor from '@/extensions/FontColor';
-import Font from '@/extensions/Font';
-import Italic from '@/extensions/Italic';
-import Underline from '@/extensions/Underline';
-import BGColor from '@/extensions/BGColor';
-import { Message, 
+import { Editor, EditorContent } from "tiptap";
+import Bold from "@/extensions/Bold";
+import Placeholder from "@/extensions/Placeholder";
+import History from "@/extensions/History";
+import Blockquote from "@/extensions/Blockquote";
+import BulletList from "@/extensions/BulletList";
+import HardBreak from "@/extensions/HardBreak";
+import Heading from "@/extensions/Heading";
+import Image from "@/extensions/Image";
+import ListItem from "@/extensions/ListItem";
+import OrderedList from "@/extensions/OrderedList";
+import HorizontalRule from "@/extensions/HorizontalRule";
+import LineHeight from "@/extensions/LineHeight";
+import { Table, TableHeader, TableRow, TableCell } from "@/extensions/Table";
+import Link from "@/extensions/Link";
+import FontSize from "@/extensions/FontSize";
+import FontFamily from "@/extensions/FontFamily";
+import FontColor from "@/extensions/FontColor";
+import Font from "@/extensions/Font";
+import Italic from "@/extensions/Italic";
+import Underline from "@/extensions/Underline";
+import BGColor from "@/extensions/BGColor";
+import {
+  Message,
   Handler,
-  SetUpdateTimeData, 
+  SetUpdateTimeData,
   OnUpdateData,
-  OnTransactionData, 
+  OnTransactionData,
   SetFontSizeData,
   SetColorData,
   InsertImageData,
   SetContentData,
   GetInitContentData,
-  GetContentData} from './NoteBridge'
+  GetContentData,
+} from "./NoteBridge";
 
 const HeaderHeight = 18;
 
 @Component({
   components: {
-    EditorContent
-  }
+    EditorContent,
+  },
 })
 export default class App extends Vue {
-
-  editor: Editor | null = null
-  updateTime = ""
+  editor!: Editor;
+  updateTime = "";
 
   beforeCreate() {
     this.$bridge.registerVoidHandler<SetUpdateTimeData>(Handler.SetUpdateTime, async (request) => {
-      this.updateTime = request.param.updateTime
-    })
+      this.updateTime = request.param.updateTime;
+    });
 
     this.$bridge.registerVoidHandler<never>(Handler.ToggleBold, async (request) => {
-      this.editor?.commands.bold()
-    })
+      this.editor?.commands.bold();
+    });
 
     this.$bridge.registerVoidHandler<never>(Handler.ToggleBulletList, async (request) => {
-      this.editor?.commands.bullet_list()
-    })
+      this.editor?.commands.bullet_list();
+    });
 
     this.$bridge.registerVoidHandler<never>(Handler.ToggleOrderedList, async (request) => {
-      this.editor?.commands.ordered_list()
-    })
+      this.editor?.commands.ordered_list();
+    });
     this.$bridge.registerVoidHandler<SetFontSizeData>(Handler.SetFontSize, async (request) => {
       this.editor?.commands.fontSize({
-        "fontSize": `${request.param.fontSize}px`
-      })
-    })
+        fontSize: `${request.param.fontSize}px`,
+      });
+    });
     this.$bridge.registerVoidHandler<SetColorData>(Handler.SetColor, async (request) => {
       this.editor?.commands.fontColor({
-        "color": request.param.color
-      })
-    })
+        color: request.param.color,
+      });
+    });
     this.$bridge.registerVoidHandler<InsertImageData>(Handler.InsertImage, async (request) => {
       this.editor?.commands.image({
-        "src": request.param.src,
-        "thumbnail": request.param.thumbnail,
-      })
-    })
+        src: request.param.src,
+        thumbnail: request.param.thumbnail,
+      });
+    });
 
     this.$bridge.registerVoidHandler<SetContentData>(Handler.SetContent, async (request) => {
-      this.editor?.setContent(request.param.content, false, undefined)
-    })
+      this.editor?.setContent(request.param.content, false, undefined);
+    });
 
     this.$bridge.registerVoidHandler<never>(Handler.Focus, async (request) => {
-      this.editor?.focus()
-    })
+      this.editor?.focus();
+    });
     this.$bridge.registerVoidHandler<never>(Handler.Blur, async (request) => {
-      this.editor?.blur()
-    })
+      this.editor?.blur();
+    });
 
     this.$bridge.registerHandler<never, GetContentData>(Handler.GetJson, async (request) => {
       return {
         id: request.id,
         code: 0,
         data: {
-          content: JSON.stringify(this.editor?.getJSON())
+          content: JSON.stringify(this.editor?.getJSON()),
         },
-      }
-    })
+      };
+    });
 
     this.$bridge.registerHandler<never, GetContentData>(Handler.GetHtml, async (request) => {
       return {
         id: request.id,
         code: 0,
         data: {
-          content: this.editor?.getHTML()
+          content: this.editor?.getHTML(),
         },
-      }
-    })
+      };
+    });
   }
 
-  async mounted() {
-    //const initContent = (await this.$bridge.sendMessage<any, GetInitContentData>(Message.GetInitContent, {})).content
-    const initContent = ""
+  created() {
     this.editor = new Editor({
-      content: initContent,
+      content: "",
       onUpdate: () => {
-        console.log('onUpdate...')
+        console.log("onUpdate...");
 
         this.$bridge.sendMessage<OnUpdateData, never>(Message.OnUpdate, {
-          json: JSON.stringify(this.editor?.getJSON() ?? {})
-        })
+          json: JSON.stringify(this.editor?.getJSON() ?? {}),
+        });
       },
       onTransaction: () => {
         this.$bridge.sendMessage<OnTransactionData, never>(Message.OnTransaction, {
           isBold: this.editor?.isActive.bold() ?? false,
           isBulletList: this.editor?.isActive.bullet_list() ?? false,
           isOrderedList: this.editor?.isActive.ordered_list() ?? false,
-        })
+        });
       },
       injectCSS: true,
-      extensions:[
+      extensions: [
         new Blockquote(),
         new BulletList(),
         new HardBreak(),
@@ -158,31 +157,36 @@ export default class App extends Vue {
         new Underline(),
         new BGColor(),
         new History(),
-        new Placeholder()
-      ]
-    })
-    
-    const isEmptyContent = (initContent == null || (typeof initContent === 'string' && initContent.length == 0))
+        new Placeholder(),
+      ],
+    });
+  }
+
+  async mounted() {
+    //const initContent = (await this.$bridge.sendMessage<any, GetInitContentData>(Message.GetInitContent, {})).content
+    let initContent = "";
+
+    const isEmptyContent = initContent == null || (typeof initContent === "string" && initContent.length == 0);
     if (isEmptyContent) {
-      this.editor.commands.bold()
-      this.editor.commands.fontSize({"fontSize": "24px", "toggle": true})
+      this.editor.commands.bold();
+      this.editor.commands.fontSize({ fontSize: "24px", toggle: true });
     }
-    this.$nextTick(function(){
+    this.$nextTick(function() {
       (this.$refs.editor as Element).scrollTo(0, HeaderHeight);
       setTimeout(() => {
-        this.editor?.focus()
+        this.editor?.focus();
         if (isEmptyContent) {
-          this.editor?.commands.bold()
-          this.editor?.commands.fontSize({"fontSize": "24px", "toggle": true})
+          this.editor?.commands.bold();
+          this.editor?.commands.fontSize({ fontSize: "24px", toggle: true });
         }
       }, 500);
     });
-    this.$bridge.sendEmptyMessage(Message.OnLoad)
-    // window.editor = this.editor 
+    this.$bridge.sendEmptyMessage(Message.OnLoad);
+    // window.editor = this.editor
   }
 
   beforeDestroy() {
-    this.editor?.destroy()
+    this.editor?.destroy();
   }
 
   render() {
@@ -194,6 +198,6 @@ export default class App extends Vue {
         <editor-content class="editor__content" editor={this.editor} />
         <div id="footer" style="height:0;"></div>
       </div>
-    )
+    );
   }
 }
